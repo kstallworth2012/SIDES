e48c635345012d323aebd7dec81f4764198dd02f
kstallworth2012 <kennethstallworth@hotmail.com>
2022-12-26 18:15:40 -0500

    intial code for action and assignment

---
+++ js/StaffingAction.js
@@ -0,0 +1,11 @@
+
+
+
+class StaffingAction {
+	constructor(staffActionID,reasonCode, actionSubject,StaffingActionContact){
+		this.staffActionID = staffActionID;
+		this.reasonCode = reasonCode; 
+		this.actionSubject = actionSubject;
+		this.StaffingActionContact =  StaffingActionContact;
+	}
+}
---
+++ js/StaffingAssignment.js
@@ -0,0 +1,14 @@
+
+
+
+class StaffingAssignment {
+	constructor(assignmentID,status,dateRange,endComments,ContractInfo,JobInfo,humanResourceInfo){
+		this.assignmentID = assignmentID;
+		this.status = status;
+		this.dateRange = dateRange;
+		this.endComments = endComments;
+		this.ContractInfo = ContractInfo;
+		this.JobInfo = JobInfo;
+		this.humanResourceInfo = humanResourceInfo;
+	}
+}
