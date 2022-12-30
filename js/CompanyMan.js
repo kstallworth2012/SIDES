503af07a9ef0207698f2c42515b0cc46d27ec0b8
kstallworth2012 <kennethstallworth@hotmail.com>
2022-12-30 09:07:28 -0500

    implementing  CompanyMan.js getters,setters and static method validate

--- js/CompanyMan.js
+++ js/CompanyMan.js
@@ -15,7 +15,7 @@ class CompanyMan {
 				linkedin,  
 				notes){
 
-				this.companyID companyID
+				this.companyID = companyID
 				this.lastName  = lastName
 				this.firstName firstName
 				this.title = title 
@@ -35,71 +35,115 @@ class CompanyMan {
 			 }
 
 
-	           get companyID() {}
+	           get companyID() {
+	           	   return this._companyID
+	           }
 
-	           set companyID(value) {}
+	           set companyID(value) {
+	           	 if(typeof value === 'string' && value.trim().length > 0){
+	           	 	this._companyID = value 
+	           	 }
+	           	   else {
+	           	   	   throw new TypeError("Value needs to be a string and length greater than zero")
+	           	   }
+	           }
 
 
 
-				get lastName() {}
+				get lastName() {
+					return this._lastName
+				}
  
 				set lastName(value) {}
 
 
-				get firstName() {}
+				get firstName() {
+					  this._firstName
+				}
 
 				set firstName(value) {}
 
 
 
-				get title() {}
+				get title() {
+					return this._title
+				}
 
 				set title(value) {}
 
 
 
-				get companyName() {}
+				get companyName() {
+					this._companyName
+				}
 
 				set companyName(value) {}
 
 
-				get address() {}
+				get address() {
+					   this._address
+				}
 
 				set address(value) {}
 
 
-				get homePhone() {}
+				get homePhone() {
+					return this._homePhone
+				}
 
 				set homePhone(value) {}
 
 
-				get workPhone() {}
+				get workPhone() {
+					this_.workPhone
+				}
 
 				set workPhone(value) {}
 
 
 
-				get fax() {}
+				get fax() {
+					this._fax
+				}
 
 				set fax(value) {}
 
 				
-				get email() {}
+				get email() {
+					this._email
+				}
 
 				set email(value) {}
 
 				
-				get webpage() {}
+				get webpage() {
+					this._webpage
+				}
 
 				set webpage(value) {}
 
 				
 				get linkedin() {}
   
-				set linkedin(value) {}
+				set linkedin(value) {
+					this._linkedin
+				}
   
 
-				get notes() {}
+				get notes() {
+					this._notes
+				}
 				set notes(value) {}
+
+				static validate(CompanyMan){
+
+						if(typeof CompanyMan.companyName === 'string' && typeof CompanyMan.firstName === 'string' && typeof CompanyMan.lastName === 'string' )
+						{
+									return
+						} 
+						else{
+							throw new TypeError("you must provide first, last names and companyName")
+						}
+				}
 
 }
